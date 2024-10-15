7a6cc1e4650b12dbf8eba2525c98f7f3a6846f19
Viktor Shubert <warreignn@gmail.com>
2024-10-16 00:23:40 +0200

    add int vector type

--- sgl-cpp/include/math/vector.h
+++ sgl-cpp/include/math/vector.h
@@ -12,4 +12,9 @@ namespace sgl
     typedef Vector<2, double> vec2d;
     typedef Vector<3, double> vec3d;
     typedef Vector<4, double> vec4d;
+
+    typedef Vector<2, int> vec2i;
+    typedef Vector<3, int> vec3i;
+    typedef Vector<4, int> vec4i;
+
 }
