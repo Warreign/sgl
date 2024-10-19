15192e8c6a12fe4ce8c207a1d953f1ebe24ab32a
Viktor Shubert <warreignn@gmail.com>
2024-10-20 00:12:57 +0200

    add point distance calculation

--- sgl-cpp/include/math/utils.h
+++ sgl-cpp/include/math/utils.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "math/vector.h"
+#include "internal/vector_internal.h"
 
-#include <type_traits>
+#include <cmath>
 
 namespace sgl 
 {
@@ -10,8 +10,8 @@ namespace sgl
 namespace math 
 {
 
-    template <size_t N, typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-    T dotProduct(const Vector<N, T>& v1, const Vector<N, T>& v2)
+    template <size_t N, typename T>
+    constexpr T dotProduct(const Vector<N, T>& v1, const Vector<N, T>& v2)
     {
         T ret = static_cast<T>(0);
         for (int i = 0; i < N; ++i)
@@ -19,6 +19,13 @@ namespace math
             ret += v1.m_data[i] * v2.m_data[i];
         }
         return ret;
+    }
+
+    template <size_t N, typename T>
+    constexpr T distance(const Vector<N, T>& p1, const Vector<N, T>& p2)
+    {
+        Vector<N, T> sub = p2 - p1;
+        return std::sqrt(dotProduct(sub, sub));
     }
 
 }
