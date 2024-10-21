ba62319825a4a70864ef4740557e69a307fe6ac0
Viktor Shubert <warreignn@gmail.com>
2024-10-21 13:31:34 +0200

    add more transformations

--- sgl-cpp/include/math/transform.h
+++ sgl-cpp/include/math/transform.h
@@ -6,63 +6,17 @@
 namespace sgl
 {
 
-    constexpr mat4 translate(vec3 direction);
-    constexpr mat4 translate(float dx, float dy, float dz);
+    mat4 translate(const vec3& direction);
+    mat4 translate(float dx, float dy, float dz);
 
-    constexpr mat4 scale(vec3 scale);
-    constexpr mat4 scale(float scaleX, float scaleY, float scaleZ);
-
-    constexpr mat4 rotate(float angleX, float angleY, float angleZ);
-    constexpr mat4 rotate(vec3 axis, float angle);
-
-    constexpr mat4 ortho(float left, float right, float bottom, float top, float near, float far);
-    constexpr mat4 perspective(float left, float right, float bottom, float top, float near, float far);
-    constexpr mat4 viewport(int x, int y, int width, int height);
-
-    inline constexpr mat4 translate(vec3 direction)
-    {
-        mat4 ret = mat4::identity;
-        vec4 translate(direction, 1);
-        ret[3] = translate;
-        return ret;
-    }
-
-    inline constexpr mat4 translate(float dx, float dy, float dz)
-    {
-        mat4 ret = mat4::identity;
-        vec4 translate(dx, dy, dz, 1);
-        ret[3] = translate;
-        return ret;
-    }
-
-    inline constexpr mat4 scale(vec3 scale)
-    {
-        mat4 ret = mat4::identity;
-        ret[0][0] = scale[0];
-        ret[1][1] = scale[1];
-        ret[2][2] = scale[2];
-        return ret;
-    }
-
-    inline constexpr mat4 scale(float scaleX, float scaleY, float scaleZ)
-    {
-        mat4 ret = mat4::identity;
-        ret[0][0] = scaleX;
-        ret[1][1] = scaleY;
-        ret[2][2] = scaleZ;
-        return ret;
-    }
-
-    inline constexpr mat4 rotate(float angleX, float angleY, float angleZ)
-    {
-        return mat4::identity;
-    }
-
-    inline constexpr mat4 rotate(vec3 axis, float angle)
-    {
-        return mat4::identity;
-    }
-
-
-
+    mat4 scale(const vec3& scale);
+    mat4 scale(float scaleX, float scaleY, float scaleZ);
+    
+    mat4 rotateZ(float angle);
+    mat4 rotateY(float angle);
+    mat4 rotate(const vec3& axis, float angle);
+    
+    mat4 ortho(float left, float right, float bottom, float top, float near, float far);
+    mat4 perspective(float left, float right, float bottom, float top, float near, float far);
+    mat4 viewport(int x, int y, int width, int height);
 }
---
+++ sgl-cpp/src/transform.cpp
@@ -0,0 +1,70 @@
+#include "math/transform.h"
+
+#include <cmath>
+
+namespace sgl
+{
+    mat4 translate(const vec3& direction)
+    {
+        mat4 ret = mat4::identity;
+        vec4 translate(direction, 1);
+        ret[3] = translate;
+        return ret;
+    }
+
+    mat4 translate(float dx, float dy, float dz)
+    {
+        mat4 ret = mat4::identity;
+        vec4 translate(dx, dy, dz, 1);
+        ret[3] = translate;
+        return ret;
+    }
+
+    mat4 scale(const vec3& scale)
+    {
+        mat4 ret = mat4::identity;
+        ret[0][0] = scale[0];
+        ret[1][1] = scale[1];
+        ret[2][2] = scale[2];
+        return ret;
+    }
+
+    mat4 scale(float scaleX, float scaleY, float scaleZ)
+    {
+        mat4 ret = mat4::identity;
+        ret[0][0] = scaleX;
+        ret[1][1] = scaleY;
+        ret[2][2] = scaleZ;
+        return ret;
+    }
+
+    mat4 rotateZ(float angle)
+    {
+        return mat4(std::cos(angle), -std::sin(angle), 0, 0,
+                    std::sin(angle),  std::cos(angle), 0, 0,
+                    0,                    0,                  1, 0,
+                    0,                    0,                  0, 1);
+    }
+
+    mat4 rotateY(float angle)
+    {
+        return mat4(std::cos(angle), 0, -std::sin(angle), 0,
+                    0,                   1, 0,                   0,
+                    std::sin(angle), 0,  std::cos(angle), 0,
+                    0,                   0,  0,                  0);
+    }
+
+    mat4 rotate(const vec3& axis, float angle)
+    {
+        return mat4::identity;
+    }
+
+    mat4 ortho(float left, float right, float bottom, float top, float near, float far) 
+    {
+        return mat4(2/(right-left), 0, 0, -(right+left)/(right-left),
+                    0, 2/(top-bottom), 0, -(top+bottom)/(top-bottom),
+                    0, 0, -2/(far-near), -(far+near)/(far-near),
+                    0, 0, 0, 1);
+    }
+
+} // namespace sgl
