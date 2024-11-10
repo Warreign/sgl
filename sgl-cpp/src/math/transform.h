fd99551fb60a3c529827d45adc9598d9cf1ee798
Warreign <warreignn@gmail.com>
2024-11-10 20:46:39 +0100

    change frustum transform

--- sgl-cpp/src/math/transform.h
+++ sgl-cpp/src/math/transform.h
@@ -81,8 +81,8 @@ namespace sgl
 
     constexpr mat4 frustum(float left, float right, float bottom, float top, float near, float far)
     {
-        return mat4((2*near)/(right-left),  0,                      0,                          -near*(right+left)/(right-left),
-                    0,                      (2*near)/(top-bottom),  0,                          -near*(top+bottom)/(top-bottom),
+        return mat4((2*near)/(right-left),  0,                      (right+left)/(right-left),  0,
+                    0,                      (2*near)/(top-bottom),  (top+bottom)/(top-bottom),  0,
                     0,                      0,                      -(far+near)/(far-near),     2*far*near/(near-far),
                     0,                      0,                      -1,                         0);
     }
