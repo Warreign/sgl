2e6b3cecef00a1a4c2b3d94301a4343ab2957db7
Viktor Shubert <warreignn@gmail.com>
2024-12-04 01:17:44 +0100

    expand primitive representation

--- sgl-cpp/src/context/primitive.cpp
+++ sgl-cpp/src/context/primitive.cpp
@@ -1,4 +1,5 @@
 #include "primitive.h"
+#include "math/utils.h"
 #include <algorithm>
 
 namespace sgl
@@ -25,13 +26,19 @@ vec3 Polygon::intersect(const Ray& ray)
 // Triangle
 Triangle::Triangle(const Material& material, const vec3& v1, const vec3& v2, const vec3& v3)
     : Primitive(material), 
-      m_vertices({v1, v2, v3})
+      m_vertices({v1, v2, v3}),
+      m_normal(math::normalize(math::crossProduct(v2-v1, v3-v1)))
 {
 }
 
-std::tuple<bool, vec3> Triangle::intersect(const Ray& ray)
+std::tuple<bool, vec3> Triangle::intersect(const Ray& ray) const
 {
     return {false, {}};
+}
+
+vec3 Triangle::getNormal(const vec3& point) const
+{
+    return m_normal;    
 }
 
 
@@ -43,9 +50,14 @@ Sphere::Sphere(const Material& material, const vec3& center, float radius)
 {
 }
 
-std::tuple<bool, vec3> Sphere::intersect(const Ray& ray)
+std::tuple<bool, vec3> Sphere::intersect(const Ray& ray) const
 {
     return {false, {}};
+}
+
+vec3 Sphere::getNormal(const vec3& point) const
+{
+    return math::normalize(point - m_center);
 }
 
 } // namespace sgl
